eabeabbfcb48018ea3d9e2434059fe1129535ce3
jacobpalaoro <jpalaoro197@gmail.com>
2022-09-18 22:52:00 -0700

    web scraper now pulls the html from ama live scoring website.

--- Web-Scraper/index.js
+++ Web-Scraper/index.js
@@ -4,4 +4,10 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 
+const url1 = 'http://americanmotocrosslive.com/'
+
+axios(url1).then(response => {
+  const html = response.data
+  console.log(html)
+})
 app.listen(PORT, () => console.log(`Express server listening on PORT ${PORT}`))
